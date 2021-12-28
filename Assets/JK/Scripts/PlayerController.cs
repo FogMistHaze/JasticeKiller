c90e6a556b2d2965fd2959f90350a3866478c5b2
Matsuhashi <da2gp.amber@gmail.com>
2021-12-28 21:25:25 +0900

    Player

--- Assets/JK/Scripts/PlayerController.cs
+++ Assets/JK/Scripts/PlayerController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 pos;
+    //private Rigidbody rb;
+    private Animator anim;
+
+    //public float speed = 2;
+
+    private const string key_isJump = "isJump";
+    private const string key_isAttack = "isAttack";
+
     void Start()
     {
-        
+        this.anim = GetComponent<Animator>();
+        //rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.position = new Vector3(pos.x+1, pos.y, pos.z);
+
+        //ジャンプ
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.anim.SetBool(key_isJump, true);
+        }
+        else
+        {
+            this.anim.SetBool(key_isJump, false);
+        }
+
+        //攻撃
+        if (Input.GetKey(KeyCode.E))
+        {
+            this.anim.SetBool(key_isAttack, true);
+        }
+        else
+        {
+            this.anim.SetBool(key_isAttack, false);
+        }
     }
 }
