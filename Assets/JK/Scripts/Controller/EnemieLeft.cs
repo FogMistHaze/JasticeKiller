aab28138ea9ed768fea10be18b92ee3cf03bffa2
Matsuhashi <da2gp.amber@gmail.com>
2021-12-30 19:41:12 +0900

    Animation、追従、操作

---
+++ Assets/JK/Scripts/Controller/EnemieLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieLeft : MonoBehaviour
+{
+    private Animator anim;
+    private const string Die = "Die";
+    private const string ATK = "isAttack";
+
+    public float speed = 0.1f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Sei")
+        {
+            anim.SetBool("Die", true);
+        }
+    }
+}
---
+++ Assets/JK/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 pos;
+    //private Rigidbody rb;
+    private Animator anim;
+
+    private const string LA = "isLeftAttack";
+    private const string RA = "isRightAttack";
+    private const string UA = "isUpAttack";
+    private const string Dam = "isDamage";
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W) ||Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetBool(UA, true);
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetBool(LA, true);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetBool(RA, true);
+        }
+        
+        /*
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            this.anim.SetBool(Dam, true);
+        }
+        */
+    }
+}
